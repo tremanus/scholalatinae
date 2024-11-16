1334811d15676725786469015b631f9a6bbba5f2
tremanus <lbiyabrook@gmail.com>
2024-11-15 18:06:02 -0800

    front end demon

--- src/components/home.js
+++ src/components/home.js
@@ -25,11 +25,17 @@ const Home = () => {
   return (
     <section className="home">
       <div className="home-content">
+        <div className="left-content">
         <h1>Schola Latinae</h1>
-        <h2>A free and fun way to learn Latin & Greek</h2>
+        <h3 className="special">/skɔː.lɑ lɑti.naɪ/</h3>
+        <img src='/home.png' alt='Home Logo' />
+        </div>
+        <div className="right-content">
+        <h2 className="description">A <b className='special'>free</b> and <b className='special'>fun</b> way to learn Latin & Greek</h2>
         <div className="button-container">
-          <button className="view-plans-button" onClick={handleViewPlansClick}>View plans</button>
-          <button className="try-button" onClick={handlePricingClick}>Try for free</button>
+          <button className="view-plans-button" onClick={handleViewPlansClick}>Get Started</button>
+          <button className="try-button" onClick={handlePricingClick}>I Have An Account</button>
+        </div>
         </div>
       </div>
     </section>
