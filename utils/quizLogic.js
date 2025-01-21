21bf399c588a90db03aae687a5a89174191b741e
tremanus <lbiyabrook@gmail.com>
2025-01-20 23:56:11 -0800

    env added

--- utils/quizLogic.js
+++ utils/quizLogic.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 const DIFFICULTY_LEVELS = {
